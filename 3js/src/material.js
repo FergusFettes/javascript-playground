1016d5c12562a058fe5debfb3128a0b33c350c53
fergus <fergusfettes@gmail.com>
2021-02-06 21:33:52 +0100

    starting to refactor the 3js source stuff

--- 3js/src/material.js
+++ 3js/src/material.js
@@ -20,13 +20,6 @@ loadManager = new THREE.LoadingManager();
 loader = new THREE.TextureLoader(loadManager);
 loader.crossOrigin = '';
 
-// const imageDict = {
-//   "https://storage.googleapis.com/schau-wien-images/media/sope2.jpg": "https://experiments.schau-wien.at/sophie/",
-//   "https://storage.googleapis.com/schau-wien-images/media/kiki1.jpg": "https://experiments.schau-wien.at/kiki/",
-//   "https://storage.googleapis.com/schau-wien-images/media/sopherfugs.jpg": "https://experiments.schau-wien.at/fergus/",
-//   "https://storage.googleapis.com/schau-wien-images/media/reeks2.jpg": "https://experiments.schau-wien.at/enrique/",
-// }
-
 const imageDict = {
   "https://storage.googleapis.com/schau-wien-images/media/sope2.jpg": "sophie",
   "https://storage.googleapis.com/schau-wien-images/media/kiki1.jpg": "kiki",
