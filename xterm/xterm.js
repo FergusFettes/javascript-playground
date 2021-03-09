7a52004dfe25b8be2496f8c86afae433249e92f9
fergus <fergusfettes@gmail.com>
2021-03-10 00:40:58 +0100

    simplified xterm

--- xterm/xterm.js
+++ xterm/xterm.js
@@ -1,36 +1,22 @@
 import { Terminal } from "xterm";
-import React from 'react';
-import { render } from 'react-dom';
 
-const styles = {
-  fontFamily: 'sans-serif',
-  textAlign: 'center',
-};
-
-const App = () => (
-  <div style={styles}>
-  </div>
-);
 const term = new Terminal();
-
-render(<App />, document.getElementById('root'));
 term.open(document.getElementById("terminal"));
-term.write("Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ");
 
-const shellprompt = '$ ';
-term.prompt = function () {
-  term.write('\r\n' + shellprompt);
+const shellprompt = "$ ";
+term.prompt = function() {
+  term.write("\r\n" + shellprompt);
 };
 
-term.writeln('Welcome to xterm.js');
-term.writeln('');
+term.write("Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ");
+term.writeln("");
 term.prompt();
-term.setOption('cursorBlink', true);
+term.setOption("cursorBlink", true);
 
 term.onKey(ev => {
   console.log(ev);
   term.write(ev.key);
-  if ( ev.key === '\r' ) {
+  if (ev.key === "\r") {
     term.prompt();
   }
 });
