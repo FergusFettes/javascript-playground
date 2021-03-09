6aad6da02d914d5d0386990da88880be766a5fa3
fergus <fergusfettes@gmail.com>
2021-03-10 00:09:32 +0100

    further with basic xterm-- mixing with react

--- xterm/xterm.js
+++ xterm/xterm.js
@@ -7,7 +7,6 @@ const styles = {
   textAlign: 'center',
 };
 
-
 const App = () => (
   <div style={styles}>
   </div>
@@ -15,47 +14,23 @@ const App = () => (
 const term = new Terminal();
 
 render(<App />, document.getElementById('root'));
-var shellprompt = '$ ';
 term.open(document.getElementById("terminal"));
 term.write("Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ");
 
+const shellprompt = '$ ';
 term.prompt = function () {
   term.write('\r\n' + shellprompt);
 };
 
 term.writeln('Welcome to xterm.js');
-term.writeln('This is a local terminal emulation, without a real terminal in the back-end.');
-term.writeln('Type some keys and commands to play around.');
 term.writeln('');
 term.prompt();
 term.setOption('cursorBlink', true);
 
-var cmd = '';
-
-term.on('key', function (key, ev) {
-  var printable = (
-    !ev.altKey && !ev.altGraphKey && !ev.ctrlKey && !ev.metaKey
-  );
-
-  if (ev.keyCode == 13) {
-    if(cmd === 'clear')
-    {
-      term.clear();
-    }
-    cmd = '';
+term.onKey(ev => {
+  console.log(ev);
+  term.write(ev.key);
+  if ( ev.key === '\r' ) {
     term.prompt();
-  } else if (ev.keyCode == 8) {
-    // Do not delete the prompt
-    console.log(term.rows);
-    if (term.x > 2) {
-      term.write('\b \b');
-    }
-  } else if (printable) {
-    cmd += key;
-    term.write(key);
   }
-});
-
-term.on('paste', function (data, ev) {
-  term.write(data);
 });
